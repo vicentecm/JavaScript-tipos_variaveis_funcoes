8dd7c791bb0782098bb3da5963e88b56a5ee9e1c
Vicente Couto Marczewski <vicentemarczewski@hotmail.com>
2022-07-28 20:33:12 -0300

    teste vsc

--- type-number.js
+++ type-number.js
@@ -1,10 +1,7 @@
 // tipo Number
 
 const meuNumero = 3;
-
 const primeiroNumero = 1;
 const segundoNumero = 'cavalo';
-
 const operacaoMatematica = primeiroNumero / segundoNumero;
-
 console.log(operacaoMatematica);
