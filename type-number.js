c6225c75115c6ef23191b35c9cc59bded108bdd6
Vicente Couto Marczewski <vicentemarczewski@hotmail.com>
2022-07-28 20:41:49 -0300

    aprendendo vsc

--- type-number.js
+++ type-number.js
@@ -2,6 +2,6 @@
 
 const meuNumero = 3;
 const primeiroNumero = 1;
-const segundoNumero = 'cavalo';
+const segundoNumero = 2;
 const operacaoMatematica = primeiroNumero / segundoNumero;
 console.log(operacaoMatematica);
